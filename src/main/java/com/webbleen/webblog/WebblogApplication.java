c238178646e385352e28c40a54e19ac986e718d8
WENBIN.LI <webbleen@gmail.com>
2020-06-06 01:56:00 +0900

    Integrate log4j2

--- src/main/java/com/webbleen/webblog/WebblogApplication.java
+++ src/main/java/com/webbleen/webblog/WebblogApplication.java
@@ -1,13 +1,18 @@
 package com.webbleen.webblog;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebblogApplication {
 
+    private static Logger logger = LoggerFactory.getLogger(WebblogApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(WebblogApplication.class, args);
+        logger.info("Spring Boot启动成功!!!!!!!!!!!!!!!!!!!!");
     }
 
 }
--- src/main/java/com/webbleen/webblog/controller/UserController.java
+++ src/main/java/com/webbleen/webblog/controller/UserController.java
@@ -2,14 +2,16 @@ package com.webbleen.webblog.controller;
 
 import com.webbleen.webblog.dao.UserMapper;
 import com.webbleen.webblog.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     UserMapper userMapper;
